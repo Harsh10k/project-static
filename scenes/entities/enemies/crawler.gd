b6e16dedeaa0174b258ca4103d08a618aaa1c2d2
nilay <nilaymehta1405@gmail.com>
2026-01-10 00:34:10 +0530

    spider enemy

---
+++ scenes/entities/enemies/crawler.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var direction: Vector2
+@export var speed: int = 225
+var player: CharacterBody2D
+
+func _on_detection_area_body_entered(player_body: CharacterBody2D) -> void:
+	player = player_body
+
+func _physics_process(delta: float) -> void:
+	if player:
+		var dir = (player.position - position).normalized()
+		velocity = dir * speed
+		move_and_slide()
+
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
