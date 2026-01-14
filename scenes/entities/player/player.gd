cc7df5a93336967f751beeb53bb4f61c47aa4f91
Harsh10k <harsh.4542.w@gmail.com>
2026-01-14 13:22:48 +0530

    Minor bug fixes

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,40 +1,42 @@
 extends CharacterBody2D
 
-
 @export var bullet_scene: PackedScene
 @export var fire_rate: float = 0.2
-
-@onready var muzzle = $Marker2D
-@onready var fire_timer = Timer.new()
-
 @export var speed: int = 500
+
+@onready var muzzle: Marker2D = $Marker2D
+@onready var fire_timer: Timer = Timer.new()
+
 var input_direction: Vector2
 
 
-func _ready():
+func _ready() -> void:
 	fire_timer.wait_time = fire_rate
 	fire_timer.one_shot = true
 	add_child(fire_timer)
 
+
 func _physics_process(_delta: float) -> void:
+	# Movement
 	input_direction = Input.get_vector("left", "right", "up", "down")
-	
-	look_at(get_global_mouse_position())
-	
 	velocity = input_direction * speed
 	move_and_slide()
 
-func _input(event):
-	if event.is_action_pressed("left_click"):
+	# Continuous shooting while left click is held
+	if Input.is_action_pressed("left_click"):
 		shoot()
 
-func shoot():
+
+func shoot() -> void:
+	# Enforce fire rate
 	if not fire_timer.is_stopped():
 		return
 
 	var bullet = bullet_scene.instantiate()
 	bullet.global_position = muzzle.global_position
-	bullet.direction = (get_global_mouse_position() - muzzle.global_position).normalized()
-	get_tree().current_scene.add_child(bullet)
+	bullet.direction = (
+		get_global_mouse_position() - muzzle.global_position
+	).normalized()
 
+	get_tree().current_scene.add_child(bullet)
 	fire_timer.start()
