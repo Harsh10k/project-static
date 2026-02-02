6ca5cafdac57f3e1ce4d41f4c741204084973afd
nilay <nilaymehta1405@gmail.com>
2026-02-02 22:36:40 +0530

    scene for healthbar

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,20 +1,32 @@
 extends CharacterBody2D
 
+signal health_changed
+
 @export var bullet_scene: PackedScene
 @export var fire_rate: float = 0.2
 @export var speed: int = 500
+
+@export var max_health = 8
+@onready var current_health: int = max_health
+
+@export var knockback_power: int = 500
 
 @onready var muzzle: Marker2D = $Marker2D
 @onready var fire_timer: Timer = Timer.new()
 
 var input_direction: Vector2
-
+var last_anim_direction: String = "Down"
+var is_hurt: bool = false
+var is_attacking: bool = false
 
 func _ready() -> void:
 	fire_timer.wait_time = fire_rate
 	fire_timer.one_shot = true
 	add_child(fire_timer)
-
+	
+func increase_health():
+	if current_health >= max_health: return 
+	current_health += 1
 
 func _physics_process(_delta: float) -> void:
 	# Movement
@@ -25,7 +37,11 @@ func _physics_process(_delta: float) -> void:
 	# Continuous shooting while left click is held
 	if Input.is_action_pressed("left_click"):
 		shoot()
-
+		
+	if !is_hurt:
+		for area in hurt_box.get_overlapping_areas():
+			if area.name == "hit_box":
+				hurt_by_enemy(area)
 
 func shoot() -> void:
 	# Enforce fire rate
@@ -40,3 +56,19 @@ func shoot() -> void:
 
 	get_tree().current_scene.add_child(bullet)
 	fire_timer.start()
+	
+func hurt_by_enemy(area):
+	current_health -= 1
+	if current_health < 0:
+		current_health = max_health	
+	
+	is_hurt = true
+	health_changed.emit()
+	
+	knockback(area.get_parent().velocity)
+	is_hurt = false
+		
+func knockback(enemy_velocity: Vector2):
+	var knockback_direction = (enemy_velocity - velocity).normalized() * knockback_power
+	velocity = knockback_direction
+	move_and_slide()
